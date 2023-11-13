5f276346ab471f0524eb4f2d191f8912c11253ad
Timothyxxx <384084775@qq.com>
2023-11-14 01:32:35 +0800

    Support loading HF models(llama, codellama) to generate reward code (update metaworld)

--- code_generation/single_flow/metaworld_exp.py
+++ code_generation/single_flow/metaworld_exp.py
@@ -3,18 +3,17 @@ import os, argparse
 from code_generation.single_flow.zero_shot.generation import ZeroShotGenerator
 from code_generation.single_flow.classlike_prompt.MetaworldPrompt import METAWORLD_PROMPT
 
-
 instruction_mapping = {
-    "window-open-v2" : "Push and open a sliding window by its handle.",
-    "window-close-v2" : "Push and close a sliding window by its handle.",
-    "door-close-v2" : "Close a door with a revolving joint by pushing door's handle.",
-    "drawer-open-v2" : "Open a drawer by its handle.",
-    "drawer-close-v2" : "Close a drawer by its handle.",
-    "door-unlock-v2" : "Unlock the door by rotating the lock counter-clockwise.",
-    "sweep-into-v2" : " Sweep a puck from the initial position into a hole.",
-    "button-press-v2" : "Press a button in y coordination.",
-    "handle-press-v2" : "Press a handle down.",
-    "handle-press-side-v2" : "Press a handle down sideways.",
+    "window-open-v2": "Push and open a sliding window by its handle.",
+    "window-close-v2": "Push and close a sliding window by its handle.",
+    "door-close-v2": "Close a door with a revolving joint by pushing door's handle.",
+    "drawer-open-v2": "Open a drawer by its handle.",
+    "drawer-close-v2": "Close a drawer by its handle.",
+    "door-unlock-v2": "Unlock the door by rotating the lock counter-clockwise.",
+    "sweep-into-v2": " Sweep a puck from the initial position into a hole.",
+    "button-press-v2": "Press a button in y coordination.",
+    "handle-press-v2": "Press a handle down.",
+    "handle-press-side-v2": "Press a handle down sideways.",
 }
 
 mapping_dicts = {
@@ -27,22 +26,23 @@ mapping_dicts = {
     "self.goal_position": "self.env._get_pos_goal()",
 }
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('--TASK', type=str, default="LiftCube-v0", \
-        help="choose one task from: drawer-open-v2, drawer-close-v2, window-open-v2, window-close-v2, button-press-v2, sweep-into-v2, door-unlock-v2, door-close-v2, handle-pull-v2, handle-press-v2, handle-press-side-v2")
+                        help="choose one task from: drawer-open-v2, drawer-close-v2, window-open-v2, window-close-v2, button-press-v2, sweep-into-v2, door-unlock-v2, door-close-v2, handle-press-v2, handle-press-side-v2")
     parser.add_argument('--FILE_PATH', type=str, default=None)
+    parser.add_argument('--MODEL_NAME', type=str, default="gpt-4")
 
     args = parser.parse_args()
 
     # File path to save result
     if args.FILE_PATH == None:
-        args.FILE_PATH = "results/metaworld/{}.txt".format(args.TASK)
+        args.FILE_PATH = "results/{}/metaworld-zeroshot/{}.txt".format(args.MODEL_NAME, args.TASK)
+
     os.makedirs(args.FILE_PATH, exist_ok=True)
 
-    code_generator = ZeroShotGenerator(METAWORLD_PROMPT)
+    code_generator = ZeroShotGenerator(METAWORLD_PROMPT, args.MODEL_NAME)
     general_code, specific_code = code_generator.generate_code(instruction_mapping[args.TASK], mapping_dicts)
 
     with open(os.path.join(args.FILE_PATH, "general.py"), "w") as f:
